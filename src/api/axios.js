5dd10a9e854d7e978bf4c45cb214537869213571
bhavatharanigithub <bhavatharni.j@gmail.com>
2026-01-14 03:54:58 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // Your backend base URL
+  baseURL: import.meta.env.VITE_API_URL
+  // baseURL: "http://localhost:5000/api", // Your backend base URL
   withCredentials: true, // Important for HttpOnly cookies
 });
 

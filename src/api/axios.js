9557ec8cb5ca216bff89d9f4cf1b6f6ac9c05f4b
bhavatharanigithub <bhavatharni.j@gmail.com>
2026-01-14 15:04:43 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,24 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+//   withCredentials: true
+// });
+
+// export default API;
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true
+  baseURL: "https://gigflow-server-g2ws.onrender.com/api",
+});
+
+// ✅ Attach token automatically
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
 });
 
 export default API;
